cb634aee338b8105bf3c62a00abfb33897e2f0b7
Caio Tairone <taironesv32@gmail.com>
2022-05-16 00:37:34 -0300

    Subida com erro

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,32 +1,61 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Movie } from 'types/movie';
-import './style.css'
+import './style.css';
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { BASE_URL } from 'utils/requests';
+import {validateEmail} from 'utils/validate';
 
 
 type Props = {movieId : string}
 
 function FormCard({movieId} : Props) {
- 
+    
+const navigate = useNavigate();
+
     const [movie, setMovie] = useState<Movie>()
 
     useEffect(() => {
         axios.get(`${BASE_URL}/movies/${movieId}`)
-        .then(response =>{
+        .then(response => {
             setMovie(response.data);
-        })
-    },[movieId]
-    )
+        });
+    })
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const email = (event.target as any).email.value;
+        const score = (event.target as any).score.value;
+        
+        if (!validateEmail(email)){
+            return;
+        }
+
+
+        const config: AxiosRequestConfig = {
+            baseURL: BASE_URL,
+            method: 'PUT',
+            url: '/scores',
+            data: {
+                email: email,
+                movieId: movieId,
+                score: score
+            }
+        }
+
+        axios(config).then(response => {
+            console.log(response.data)
+            navigate("/")
+        });
+    }
 
     return (
         <div className="svmovie-form-container">
             <img className="svmovie-movie-card-image" src={movie?.image} alt={movie?.title} />
             <div className="svmovie-card-bottom-container">
                 <h3>{movie?.title}</h3>
-                <form className="svmovie-form">
+                <form className="svmovie-form" onSubmit= {handleSubmit}>
                     <div className="form-group svmovie-form-group">
                         <label htmlFor="email">Informe seu email</label>
                         <input type="email" className="form-control" id="email" />
